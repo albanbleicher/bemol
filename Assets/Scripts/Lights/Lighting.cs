5535f0ff69c41ed83035470a908d664c5877eadf
pierrepintoo <p.pinto2oliveira@gmail.com>
2021-12-01 14:58:12 +0100

    :sparkles: First scene with lightning and lighting

---
+++ Assets/Scripts/Lights/Lighting.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighting : MonoBehaviour
+{
+	private Animation anim;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        anim = GetComponent<Animation>();
+
+        while (true) {
+            yield return new WaitForSeconds(Random.Range(2,10));
+            anim.Play();
+        }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
