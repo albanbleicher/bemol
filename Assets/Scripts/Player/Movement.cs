17981f4f1792ff2bd0e562d4f84402846d9cda8f
Alban Bleicher <alban.b@thewalkingnerds.com>
2021-12-03 10:47:40 +0100

    :fire: :sparkles: :zap: camera movements, create prefabs, organize assets

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Movement : MonoBehaviour
+{
+    // Speed of Camera Movement
+    private Animator animator;
+    private NavMeshAgent agent;
+    private float pitch = 0.0f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * 2f;
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * z * agent.speed;
+        transform.Rotate(0f, x, 0f);
+        agent.Move(move * Time.deltaTime);
+        animator.SetFloat("Speed", move.magnitude);
+    }
+}
