7119b08227a81fcb052447591ab3ee3a6aec37ee
Pierre Pinto de Oliveira <p.pinto2oliveira@gmail.com>
2021-12-08 22:52:42 +0100

    :sparkles: Thunder + new oil material + oil sound

--- Assets/Scripts/Lights/Lighting.cs
+++ Assets/Scripts/Lights/Lighting.cs
@@ -6,15 +6,33 @@ public class Lighting : MonoBehaviour
 {
 	private Animation anim;
 
+    private AudioSource thunderSound;
+
+    private Light FoudreLight;
+
+    public static Lighting Instance  {get; private set; }
+
+
+    private void Awake() {
+        Instance = this;
+    }
+
     // Start is called before the first frame update
     IEnumerator Start()
     {
         anim = GetComponent<Animation>();
+        thunderSound = GetComponent<AudioSource>();
+        FoudreLight = GetComponent<Light>();
+        FoudreLight.intensity = 0;
 
         while (true) {
-            yield return new WaitForSeconds(Random.Range(2,10));
+            yield return new WaitForSeconds(Random.Range(15,25));
+            // TO DO : Working that shit
+            // ShakingController.Instance.ShakeCamera(0.0f, 0.2f, .1f);
             anim.Play();
-        }    
+            yield return new WaitForSeconds(1.5f);
+            thunderSound.Play();
+        }
     }
 
     // Update is called once per frame
